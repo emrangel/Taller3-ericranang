f8ec0fbd1d0696493e5e393b9224fc9882590c0f
unknown <erriick1122@gmail.com>
2024-11-12 21:12:02 -0500

    actu

--- models/huron.py
+++ models/huron.py
@@ -2,8 +2,5 @@
 from models import animal_exotico
 
 class Huron(animal_exotico.Animal_Exotico):
-    def __init__(self, nombre: str, peso: float, edad: int, pais_origen: str, impuestos: float) -> None:
-        super().__init__(nombre, peso, edad, pais_origen, impuestos)
-        
     def hacer_sonido(self):
         return "¡Eek Eek!"
